2dfe7fbc219fd56f2fce40739a3ca1a4a7d9c88e
Vitor Cabral <vitor.cs11@gmail.com>
2025-04-15 23:59:58 -0300

    implentando springDoc

--- src/main/java/heath/med/NeoHealthCare/security/SecurityConfigurations.java
+++ src/main/java/heath/med/NeoHealthCare/security/SecurityConfigurations.java
@@ -27,6 +27,8 @@ public class SecurityConfigurations {
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(req -> {
           req.requestMatchers("/login").permitAll();
+          req.requestMatchers("/login").permitAll();
+          req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
           req.anyRequest().authenticated();
         })
         .addFilterBefore(authSecurity, UsernamePasswordAuthenticationFilter.class)
